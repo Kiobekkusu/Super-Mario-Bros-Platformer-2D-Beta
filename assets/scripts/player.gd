f3bb0cef32c8e8e281f8ce0e3560add79d57f170
Kiovex <kiovex.510@gmail.com>
2025-03-10 01:29:29 +0100

    Update player.gd

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,18 +1,22 @@
 extends CharacterBody2D
 
+class_name Player
+
 #Variables
-@export var gravity = 450
-@export var jump_speed = 250
+@export var gravity = 500
+@export var jump_speed = 230 # 230 - 280
 @export var speed = 150
 @export var limit_speed = 200
 @export var accel = 3.0
 @export var friction = 10.0 #originalmente era 0.1 pero bruh
 
-var imgspeed = 4 # 4 - 8
+@onready var raycast = $RayCast2D
+
+var imgspeed = 4 # 4 - 8 
 var count_jump = 0; # accountant of jumps
 
 #states
-var on_floor = false
+var on_floor = false	
 var speedrun = false
 
 func _physics_process(delta):
@@ -35,12 +39,15 @@ func _physics_process(delta):
 		speed += 2
 		if speed > limit_speed:
 			speed = limit_speed
+			jump_speed = 280;
 		elif -speed < -limit_speed:
 			speed = -limit_speed
+			jump_speed = 280;
 		anim_player.speed_scale = imgspeed
 	else:
 		speed = 150
 		anim_player.speed_scale = 4
+		jump_speed = 210
 	
 	# if not move
 	if direction == 0:
@@ -80,6 +87,13 @@ func _physics_process(delta):
 	if jump_pressed and count_jump < 1:
 		velocity.y -= jump_speed
 		count_jump += 1;
+		
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		if collider is Block and not collider.is_moving:
+			collider.move_block()
+			velocity.y += jump_speed
+			
 		
 	print(velocity.x)
 		
